8e67dfac103c180cad6cfd70d08f7fac4971ad1b
nicok <khamsophan@yahoo.fr>
2023-09-25 18:25:17 +0200

    Ajout des comportements demandés dans la todo list*

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,7 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
+const closeBtn = document.querySelector(".close");
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
@@ -20,4 +20,32 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+closeBtn.addEventListener("click", closeModal);
 
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+function validate() {
+  const conditionCheckbox = document.getElementById("checkbox1");
+  const errorMessage = document.getElementById("message_conditions");
+  const prenom = document.getElementById("prenom");
+  const nom = document.getElementById("nom");
+  const email = document.getElementById("mail").value;
+  const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+  if (conditionCheckbox.checked == false) {
+    errorMessage.style.display = "block";
+    errorMessage.style.color = "red";
+    errorMessage.innerText = "Veuillez accepter les conditions pour continuer.";
+    return false;
+  }
+  if (prenom.value.length <2 || nom.value.length <2 || !email.match(emailRegex) ) {
+    alert("Veuillez remplir les champs du formulaire.")
+    return false;
+  }
+  else if (prenom.value.length >1 && prenom.value.length >1 && conditionCheckbox.checked == true) {
+    alert("Merci ! Votre réservation a été reçue.");
+    return true;
+  }
+}
